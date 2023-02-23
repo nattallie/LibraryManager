cdf3248fdc50e6be15711634bc3a8a71a7841489
Nata Khurtsidze <natakhurtsidze@gmail.com>
2023-02-23 14:41:35 +0400

    project created, gitignore added

---
+++ LibraryManager/LibraryManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryManagerApp.swift
+//  LibraryManager
+//
+//  Created by Nata Khurtsidze on 23.02.23.
+//
+
+import SwiftUI
+
+@main
+struct LibraryManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
